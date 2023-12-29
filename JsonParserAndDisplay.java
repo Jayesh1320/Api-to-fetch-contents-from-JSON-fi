cfdeedb04270008c477c2f0ac271b28c9b2d5b3d
Jayesh1320 <155161500+Jayesh1320@users.noreply.github.com>
2023-12-29 18:44:04 +0530

    Update JsonParserAndDisplay.java

--- JsonParserAndDisplay.java
+++ JsonParserAndDisplay.java
@@ -1,14 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.json.*;
 
 class Product {
     String title;
